ed08b3a612caad7c418c263472bf9df14a7aa749
unknown <syahputraryandi23@gmail.com>
2025-08-03 18:22:44 +0700

    Ubah Kriteria & subkriteria

--- list-kriteria.php
+++ list-kriteria.php
@@ -8,7 +8,15 @@ require_once('template/header.php');
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
 	<h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-cube"></i> Data Kriteria</h1>
 
-	<a href="tambah-kriteria.php" class="btn btn-success"> <i class="fa fa-plus"></i> Tambah Data </a>
+<div class="dropdown">
+  <button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">
+    Tambah Data
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="tambah-kriteria.php">Tambah Kriteria</a>
+    <a class="dropdown-item" href="list-sub-kriteria.php">Tambah Subkriteria</a>
+  </div>
+</div>
 </div>
 
 <?php
@@ -39,43 +47,59 @@ endif;
 
 	<div class="card-body">
 		<div class="table-responsive">
-			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-				<thead class="bg-danger text-white">
-					<tr align="center">
-						<th>No</th>
-						<th>Kode Kriteria</th>
-						<th>Nama Kriteria</th>
-						<th>Atribut</th>
-						<th>Bobot</th>
-						<th>Cara Penilaian</th>
-						<th width="15%">Aksi</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					$no = 1;
-					$query = mysqli_query($koneksi, "SELECT * FROM kriteria ORDER BY kode_kriteria ASC");
-					while ($data = mysqli_fetch_array($query)) :
-					?>
-						<tr align="center">
-							<td><?php echo $no; ?></td>
-							<td><?php echo $data['kode_kriteria']; ?></td>
-							<td align="left"><?php echo $data['kriteria']; ?></td>
-							<td><?php echo $data['type']; ?></td>
-							<td><?php echo $data['bobot']; ?></td>
-							<td><?php echo ($data['ada_pilihan']) ? 'Pilihan Sub Kriteria' : 'Input Langsung'; ?></td>
-							<td>
-								<div class="btn-group" role="group">
-									<a data-toggle="tooltip" data-placement="bottom" title="Edit Data" href="edit-kriteria.php?id=<?php echo $data['id_kriteria']; ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
-									<a data-toggle="tooltip" data-placement="bottom" title="Hapus Data" href="hapus-kriteria.php?id=<?php echo $data['id_kriteria']; ?>" onclick="return confirm ('Apakah anda yakin untuk meghapus data ini')" class="btn btn-primary btn-sm"><i class="fa fa-trash"></i></a>
-								</div>
-							</td>
-						</tr>
-					<?php
-						$no++;
-					endwhile; ?>
-				</tbody>
-			</table>
+<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+  <thead class="bg-danger text-white">
+    <tr align="center">
+      <th>No</th>
+      <th>Kode Kriteria</th>
+      <th>Nama Kriteria</th>
+      <th>Kode Subkriteria</th>
+      <th>Nama Subkriteria</th>
+      <th>Nilai Subkriteria</th>
+      <th>Atribut</th>
+      <th>Bobot</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $no = 1;
+    $query = mysqli_query($koneksi, "SELECT * FROM kriteria ORDER BY kode_kriteria ASC");
+    while ($kriteria = mysqli_fetch_array($query)) :
+      $sub_query = mysqli_query($koneksi, "SELECT * FROM sub_kriteria WHERE id_kriteria = '{$kriteria['id_kriteria']}' ORDER BY nilai DESC");
+      $first = true;
+      while ($sub = mysqli_fetch_array($sub_query)) :
+    ?>
+        <tr align="center">
+          <td><?= $no++ ?></td>
+          <td><?= $first ? $kriteria['kode_kriteria'] : '' ?></td>
+          <td align="left"><?= $first ? $kriteria['kriteria'] : '' ?></td>
+          <td><?= $sub['kode_sub'] ?? '' ?></td>
+          <td align="left"><?= $sub['sub_kriteria'] ?></td>
+          <td><?= $sub['nilai'] ?></td>
+          <td><?= $first ? $kriteria['type'] : '' ?></td>
+          <td><?= $first ? $kriteria['bobot'] : '' ?></td>
+          <td>
+            <?php if ($first) : ?>
+              <div class="dropdown">
+                <button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-toggle="dropdown">
+                  Edit
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="edit-kriteria.php?id=<?= $kriteria['id_kriteria'] ?>">Edit Data Kriteria</a>
+                  <a class="dropdown-item" href="list-sub-kriteria.php?id=<?= $sub['id_sub_kriteria'] ?>">Edit Data Subkriteria</a>
+                </div>
+              </div>
+            <?php endif; ?>
+          </td>
+        </tr>
+    <?php
+        $first = false;
+      endwhile;
+    endwhile;
+    ?>
+  </tbody>
+</table>
 		</div>
 	</div>
 </div>
